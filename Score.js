7085ce107424c91b1473dceffd0084056990cf6a
Frances Saunders <92679675+frances-saunders@users.noreply.github.com>
2022-09-26 12:59:28 -0400

    Create Score.js

---
+++ Score.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectMatchedIDs } from '../board/boardSlice.js';
+
+export const Score = () => {
+  const cardsMatched = useSelector(selectMatchedIDs);
+  
+
+  return (
+    // implement selected data inside <div>
+    <div className="score-container">Matched: {cardsMatched.length}</div>
+  );
+};
